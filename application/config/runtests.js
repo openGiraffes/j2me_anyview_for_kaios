4346d917df3a892b84b5bf620b552a9df21937bb
zixing <1311817771@qq.com>
2022-06-24 13:25:03 +0800

    更新框架
    更新框架

--- application/config/runtests.js
+++ application/config/runtests.js
@@ -1,5 +1,5 @@
-config.jars = "tests/Anyview4.0.jar";
-config.jad = "tests/Anyview4.0.jad";
+config.jars = "jar/Anyview4.0.jar";
+config.jad = "jar/Anyview4.0jad";
 config.midletClassName = "com.ismyway.anyview.Anyview"; 
 MIDlet.shouldStartBackgroundService = function() {
   return fs.exists("/startBackgroundService");
